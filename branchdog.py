0edeacec2b041c2103e9cc8164262c82eb59bf39
Toni Heinänen <toni.heinanen@gmail.com>
2022-06-06 15:40:49 +0300

    modified comments

--- branchdog.py
+++ branchdog.py
@@ -6,8 +6,8 @@ from pygit2 import Repository, GitError
 from pygit2 import *
 import os
 
-# Get the repository path from user input or exit if canceled
 def get_repo_path() -> str:
+    "Get the repository path from user input or exit if canceled"
     while (True):
         repo_path = filedialog.askdirectory(initialdir=os.path.normpath("C:\\"), title="Please select a repository to watch!")
         if (discover_repository(repo_path)):
@@ -23,8 +23,8 @@ main_window = Tk()
 main_window.title(f"Repo: {title[-1]}")
 main_window.attributes('-topmost', True)
 
-# Get the current active shorthand branch name as a string
 def get_branch_name() -> str:
+    "Get the current active shorthand branch name as a string"
     try:
         repo = Repository(REPO_PATH)
     except (GitError):
@@ -32,15 +32,15 @@ def get_branch_name() -> str:
     head = repo.head
     return head.shorthand
 
-# Continuously check for the branch name, updating main_text in main_window accordingly along with the background color
 def update_repo():
+    "Continuously check for the branch name, updating main_text in main_window accordingly along with the background color"
     branch_name = get_branch_name()
     main_text.configure(text=f"Currently selected branch: {branch_name}")
     main_text.configure(bg="red") if determine_master(branch_name) else main_text.configure(bg="green")
     main_window.after(1000, update_repo)
 
-# Determine if the active branch is master/main or not
 def determine_master(branch_name: str) -> bool:
+    "Determine if the active branch is master/main or not"
     return True if (re.search(r'\bmaster\b', branch_name) or re.search(r'\bmain\b', branch_name)) else False
 
 branch_name = get_branch_name()
